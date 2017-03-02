2384d836ad6631218def469a642fbaff9b997a20
Andriy Suden <andriykosud@gmail.com>
2017-03-02 16:29:52 -0600

    Initial commit for ajx

---
+++ include/vec2d.h
@@ -0,0 +1,14 @@
+#ifndef AJX_VEC2D
+#define AJX_VEC2D
+
+namespace ajx {
+    class vec2d {
+        public:
+            vec2d();
+            ~vec2d();
+        private:
+
+    };
+}
+
+#endif
---
+++ src/lib/vec2d.cpp
@@ -0,0 +1,12 @@
+#include "vec2d.h"
+
+namespace ajx {
+    vec2d::vec2d() {
+
+    };
+
+    vec2d::~vec2d() {
+
+    }
+}
+
