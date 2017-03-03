107400785219486dc28e2c37b6d6cc4a6e5077fd
Andriy Suden <andriykosud@gmail.com>
2017-03-03 15:26:11 -0600

    Fixed the compiling issue for boost operators

--- include/vec2d.h
+++ include/vec2d.h
@@ -5,7 +5,8 @@
 #include <cmath>
 
 namespace ajx {
-    template <class T> class vec2d : boost::operators< vec2d<T> > {
+    template <class T>
+    class vec2d : boost::operators< vec2d<T> > {
         public:
             vec2d(T t1, T t2) {
 				this->m_x = t1;
@@ -16,17 +17,75 @@ namespace ajx {
 
             };
 
-            bool operator<(const vec2d& x) const;
-		    bool operator==(const vec2d& x) const;
-		    vec2d operator+=(const vec2d& x);
-		    vec2d operator-=(const vec2d& x);
-		    vec2d operator*=(const vec2d& x);
-		    vec2d operator/=(const vec2d& x);
-		    vec2d operator%=(const vec2d& x);
-		    vec2d operator|=(const vec2d& x);
-		    vec2d operator&=(const vec2d& x);
-		    vec2d operator^=(const vec2d& x);
-		    vec2d operator--();
+            bool operator<(const vec2d<T>& x) const {
+            	return ((this->m_x * this->m_x + this->m_y + this->m_y) < (x.m_x * x.m_x + x.m_y * x.m_y));
+            };
+
+		    bool operator==(const vec2d<T>& x) const {
+		    	if (this->m_x != x.m_x) return false;
+		    	if (this->m_y != x.m_y) return false;
+		    	return true;
+		    };
+
+		    vec2d<T>& operator+=(const vec2d<T>& x) {
+		    	this->m_x += x.m_x;
+		    	this->m_y += x.m_y;
+		    	return *this;
+		    };
+
+		    vec2d<T>& operator-=(const vec2d<T>& x) {
+		    	this->m_x -= x.m_x;
+		    	this->m_y -= x.m_y;
+		    	return *this;
+		    };
+
+		    vec2d<T>& operator*=(const vec2d<T>& x) {
+				this->m_x = this->m_x * x.m_x;
+				this->m_y = this->m_y * x.m_y;
+				return *this;
+			};
+
+		    vec2d<T>& operator/=(const vec2d<T>& x) {
+		    	this->m_x /= x.m_x;
+		    	this->m_y /= x.m_y;
+		    	return *this;
+		    };
+
+		    vec2d<T>& operator%=(const vec2d<T>& x) {
+		    	this->m_x %= x.m_x;
+		    	this->m_y %= x.m_y;
+		    	return *this;
+		    };
+
+		    vec2d<T>& operator|=(const vec2d<T>& x) {
+		    	this->m_x |= x.m_x;
+		    	this->m_y |= x.m_y;
+		    	return *this;
+		    };
+
+		    vec2d<T>& operator&=(const vec2d<T>& x) {
+		    	this->m_x &= x.m_x;
+		    	this->m_y &= x.m_y;
+		    	return *this;
+		    };
+
+		    vec2d<T>& operator^=(const vec2d<T>& x) {
+		    	this->m_x ^= x.m_x;
+		    	this->m_y ^= x.m_y;
+		    	return *this;
+		    };
+
+		    vec2d<T>& operator++() {
+		    	this->m_x += 1;
+		    	this->m_y += 1;
+		    	return *this;
+		    };
+
+		    vec2d<T>& operator--() {
+		    	this->m_x -= 1;
+		    	this->m_y -= 1;
+		    	return *this;
+		    };
 
             T x() { return m_x; };
             T y() { return m_y; };
