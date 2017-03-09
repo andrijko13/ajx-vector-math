b99e2c7b10f2712e3d62cb2108de4df794a82a47
Andriy Suden <andriykosud@gmail.com>
2017-03-08 19:47:32 -0600

    Updated example file to work with 3d vec and showcase more features

--- src/examples/main.cpp
+++ src/examples/main.cpp
@@ -1,4 +1,5 @@
 #include "vec2d.h"
+#include "vec3d.h"
 #include <iostream>
 
 int main(int argc, char **argv) {
@@ -8,6 +9,15 @@ int main(int argc, char **argv) {
   ajx::vec2d<int> result = position1 + position2;
 
   std::cout << "X: " << result.x() << " Y: " << result.y() << std::endl;
+
+  ajx::vec3d<double> velocity (20,20,0);
+  std::cout << "Length of <20,20,0> is " << velocity.length() << std::endl;
+  velocity = velocity.normalized();
+
+  std::cout << "<20,20,0> normalized is <" << velocity.x() << ", " << velocity.y() << ", " << velocity.z() << ">" << std::endl;
+
+  std::cout << "Length normalized is " << velocity.length() << std::endl;
+
 
   return 0;
 }
