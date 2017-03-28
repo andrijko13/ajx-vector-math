70d3f741052ad4ad69561595421396150267a01b
Curt <cthieme74@gmail.com>
2017-03-28 13:19:09 -0500

    Added some more test cases for vec3d

--- src/test/test_vec3d.cpp
+++ src/test/test_vec3d.cpp
@@ -127,6 +127,42 @@ BOOST_AUTO_TEST_CASE( minus_equals )
   BOOST_CHECK(second.z() == 1);
 }
 
+BOOST_AUTO_TEST_CASE ( times_equals )
+{
+  ajx::vec3d<int> first(4,5,6);
+  ajx::vec3d<int> second(-3, 7, 5);
+  ajx::vec3d<int> third(1,1,1);
+
+  first *= second;
+  second *= third;
+
+  BOOST_CHECK(first.x() == -12);
+  BOOST_CHECK(first.y() == 35);
+  BOOST_CHECK(first.z() == 30);
+  BOOST_CHECK(second.x() == -3);
+  BOOST_CHECK(second.y() == 7);
+  BOOST_CHECK(second.z() == 5);
+}
+
+BOOST_AUTO_TEST_CASE ( divide_equals )
+{
+  ajx::vec3d<int> first(20,-20,35);
+  first /= ajx::vec3d<int>::VectorWith(5,5,7);
+
+  BOOST_CHECK(first.x() == 4);
+  BOOST_CHECK(first.y() == -4);
+  BOOST_CHECK(first.z() == 5);
+}
+
+BOOST_AUTO_TEST_CASE( modulus_equals )
+{
+  ajx::vec3d<int> first(20,-20, 40);
+  first %= ajx::vec3d<int>::VectorWith(5,3, 7);
+
+  BOOST_CHECK(first.x() == 0);
+  BOOST_CHECK(first.y() == -2);
+  BOOST_CHECK(first.z() == 5);
+}
 
 BOOST_AUTO_TEST_CASE( cross )
 {
